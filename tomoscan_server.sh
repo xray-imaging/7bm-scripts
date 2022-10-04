ed3b9d564fc720293c843be1a1bb43f045ba3224
aps-7bm <aps.7bm@gmail.com>
2022-10-04 14:15:33 -0500

    Further debugging changes for 7-BM vs. 2-BM.

--- tomoscan_server.sh
+++ tomoscan_server.sh
@@ -1,5 +1,5 @@
 gnome-terminal --tab --title "tomoScan py server" -- bash -c "ssh -t 7bmb@prandtl \
-'bash ~/scripts/kill_server.sh start_tomoscan.py;  \
+'bash ~/scripts/kill_server.sh start_tomoscan_7bm.py;  \
 cd /home/beams/7BMB/epics/synApps/support/tomoscan/iocBoot/iocTomoScan_7BM; \
-bash -c \"source ~/.bashrc; conda activate tomoscan; python -i start_tomoscan.py\";\
+bash -c \"source ~/.bashrc; conda activate tomoscan; python -i start_tomoscan_7bm.py\";\
 bash'" 
