5ef534f554f73ac32327dfb099157bf7b72ec90d
aps-7bm <aps.7bm@gmail.com>
2022-10-04 13:26:10 -0500

    Edits to ADL screen to use 7BMB as computer account.

--- tomostream_server.sh
+++ tomostream_server.sh
@@ -1,5 +1,5 @@
 gnome-terminal --tab --title "tomoStream py server" -- bash -c "ssh -t 7bmb@mach \
 'bash ~/scripts/kill_server.sh start_tomostream.py;  \
 cd /home/beams/7BMB/epics/synApps/support/tomostream/iocBoot/iocTomoStream_7BM; \
-bash -c \"source ~/.bashrc; conda activate tomostream; python -i start_tomostream.py\";\
+bash -c \"source ~/.bashrc; conda activate tomostream2; python -i start_tomostream.py\";\
 bash'" 
